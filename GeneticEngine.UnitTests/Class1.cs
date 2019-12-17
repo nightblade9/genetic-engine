77167101a2356bbe3bb9ed7357965c1f6f31c3ff
nightblade9 <nightbladecodes@gmail.com>
2019-12-17 11:08:38 -0500

    Hello World unit test Uses .NET Core since Standard isn't a runtime

---
+++ GeneticEngine.UnitTests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GeneticEngine.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void HelloWorld()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
