27bcb21ebdf2c69e0198aadf9d07b9c109e559ee
nightblade9 <nightbladecodes@gmail.com>
2020-02-06 15:39:15 -0500

    UNit tests: `Engine.Solve`

--- GeneticEngine.UnitTests/EngineTests.cs
+++ GeneticEngine.UnitTests/EngineTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using NUnit.Framework;
-using GeneticEngine;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -48,6 +47,105 @@ namespace GeneticEngine.UnitTests
             Assert.That(actual.Any(s => s.Fitness != bestFitness)); // not always best
             Assert.That(actual.Distinct().Count(), Is.GreaterThan(1)); // not always the same
             Assert.That(actual.All(s => solutions.Contains(s))); // always from our pool
+        }
+
+        [Test]
+        public void SolveThrowsIfPopulationIsNotSet()
+        {
+            var engine = new Engine<int, int>(1000, 0.1f, 0.5f, 0.1f);
+            var ex = Assert.Throws<InvalidOperationException>(() => engine.Solve());
+            Assert.That(ex.Message.Contains("CreateInitialPopulation"));
+        }
+
+        [Test]
+        public void SolveThrowsIfFitnessMethodIsNotSet()
+        {
+            // Arrange
+            var engine = new Engine<int, int>(1000, 0.1f, 0.5f, 0.1f);
+            engine.CreateInitialPopulation(() => 7);
+
+            // Act
+            var ex = Assert.Throws<InvalidOperationException>(() => engine.Solve());
+
+            // Assert
+            Assert.That(ex.Message.Contains("SetFitnessMethod"));
+        }
+
+        [Test]
+        public void SolveThrowsIfCrossOverMethodIsNotSet()
+        {
+            // Arrange
+            var engine = new Engine<int, int>(1000, 0.1f, 0.5f, 0.1f);
+            engine.CreateInitialPopulation(() => 7);
+            engine.SetFitnessMethod((a) => a + 1);
+            //engine.SetCrossOverMethod((a, b) => new List<int>() { a + 1, b - 1});
+
+            // Act
+            var ex = Assert.Throws<InvalidOperationException>(() => engine.Solve());
+
+            // Assert
+            Assert.That(ex.Message.Contains("SetCrossOverMethod"));
+        }
+
+        [Test]
+        public void SolveThrowsIfSelectionMethodIsNotSet()
+        {
+            // Arrange
+            var engine = new Engine<int, int>(1000, 0.1f, 0.5f, 0.1f);
+            engine.CreateInitialPopulation(() => 7);
+            engine.SetFitnessMethod((a) => a + 1);
+            engine.SetCrossOverMethod((a, b) => new List<int>() { a + 1, b - 1});
+
+            // Act
+            var ex = Assert.Throws<InvalidOperationException>(() => engine.Solve());
+
+            // Assert
+            Assert.That(ex.Message.Contains("SetSelectionMethod"));
+        }
+
+        [Test]
+        public void SolveThrowsIfMutationMethodIsNotSet()
+        {
+            // Arrange
+            var engine = new Engine<int, int>(1000, 0.1f, 0.5f, 0.1f);
+            engine.CreateInitialPopulation(() => 7);
+            engine.SetFitnessMethod((a) => a);
+            engine.SetCrossOverMethod((a, b) => new List<int>() { a + 1, b - 1});
+            engine.SetSelectionMethod(engine.TournamentSelection);
+
+            // Act
+            var ex = Assert.Throws<InvalidOperationException>(() => engine.Solve());
+
+            // Assert
+            Assert.That(ex.Message.Contains("SetMutationMethod"));
+        }
+
+        [Test]
+        public void SolveSolvesInTenGenerations()
+        {
+            int numGenerations = 0;
+            float bestFitness = 0;
+
+            // Arrange
+            var engine = new Engine<int, int>(1000, 0.1f, 0.5f, 0.1f);
+            engine.CreateInitialPopulation(() => 7);
+            // Guaranteed to solve quickly because of a static population of 7s and a static fitness
+            engine.SetFitnessMethod((a) => 99);
+            engine.SetCrossOverMethod((a, b) => new List<int>() { a + 1, b - 1});
+            engine.SetSelectionMethod(engine.TournamentSelection);
+            engine.SetMutationMethod((a) => a);
+
+            engine.SetOnGenerationCallback((generation, best) => {
+                bestFitness = best.Fitness;
+                numGenerations = generation;
+            });
+
+            // Act
+            engine.Solve();
+
+            // Assert: didn't throw, and gives us the expected fitness
+            Assert.That(bestFitness, Is.EqualTo(99));
+            Assert.That(numGenerations, Is.EqualTo(10));
         }
     }
 }
