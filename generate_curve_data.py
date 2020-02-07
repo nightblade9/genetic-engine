afbbd969dff678f217d446be003b30febd8098da
nightblade9 <nightbladecodes@gmail.com>
2020-02-07 10:33:56 -0500

    Add Python script to generate data-set based on equation

---
+++ generate_curve_data.py
@@ -0,0 +1,31 @@
+'''
+Given a manually-entered Python mathematical equation, picks random points from x=-1000...1000 and outputs the data for those.
+'''
+
+import csv
+import random
+
+# User-defined inputs
+MIN_X = 0
+MAX_X = 1000
+# To start: y=3x^2 - 2x + 17
+EQUATION = lambda x: 3 * (x**2) - (2 * x) + 17
+
+# Internal constants
+NUM_POINTS = 100
+OUTPUT_FILE = "data.csv"
+
+x_indicies = []
+
+while len(x_indicies) < NUM_POINTS:
+    new_index = random.uniform(MIN_X, MAX_X)
+    if not new_index in x_indicies:
+        x_indicies.append(new_index)
+
+
+with open(OUTPUT_FILE, 'wb') as out_file:
+    writer = csv.writer(out_file)
+    for x in x_indicies:
+        writer.writerow([x, EQUATION(x)])
+
+print("Wrote {} data points to {}".format(NUM_POINTS, OUTPUT_FILE))
