9ce3b4d179e15eee8a9aef621464204c4a99a59e
nightblade9 <nightbladecodes@gmail.com>
2020-02-12 11:52:15 -0500

    Non-mutation solution for curve-fitting

--- SampleSolutions/CurveFittingSolver.cs
+++ SampleSolutions/CurveFittingSolver.cs
@@ -37,7 +37,7 @@ namespace SampleSolutions
 
             this.LoadXAndExpectedValuesFromCsv();
             var tree = this.GenerateSubtree(PROBABILITY_OF_SUBTREE);
-            var engine = new Engine<OperatorNode<float>, Object>(1000, 0.1f, 0.5f, 0.1f);
+            var engine = new Engine<OperatorNode<float>, Object>(10000, 0.1f, 0.75f, 0f);
             engine.CreateInitialPopulation(this.CreateRandomTrees);
             engine.SetFitnessMethod(this.CalculateFitness);
             engine.SetCrossOverMethod(this.CrossOver);
